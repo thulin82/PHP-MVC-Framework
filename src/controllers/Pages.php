0dd1a53e1aafd92e1a1a17a95d1282df8b22732e
Markus Thulin <macky_b@hotmail.com>
2020-07-02 15:10:38 +0200

    Set up basic Views

--- src/controllers/Pages.php
+++ src/controllers/Pages.php
@@ -5,7 +5,11 @@
         }
 
         public function index(){
+            $this->view('pages/index', ['title' => 'Welcome to index page']);
+        }
 
+        public function about(){
+            $this->view('pages/about', ['title' => 'Welcome to about page']);
         }
     }
     
